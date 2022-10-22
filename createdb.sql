8c3d2274701c82ea939737894466e512f8ecfb51
Manuel Gomez <mjgomez@nevada.unr.edu>
2022-10-22 11:40:17 -0700

    AA-149 Final Database Tables

--- createdb.sql
+++ createdb.sql
@@ -1,56 +1,58 @@
 CREATE TABLE JOBTITLE (
-	JobTitleID int primary key
+	JobTitleID float primary key,
+	JobTitle varchar(30)
 );
 
 CREATE TABLE EMPLOYEETYPE (
-	EmployeeTypeID int primary key
+	EmployeeTypeID float primary key,
+	EmployeeType varchar(30)
 );
 
 CREATE TABLE COMPANY (
-	CompanyID int primary key,
+	CompanyID float primary key,
 	CompanyName varchar(50)
 );
 
 CREATE TABLE SERVICETYPE (
-	ServiceTypeID int PRIMARY KEY,
+	ServiceTypeID float PRIMARY KEY,
 	ServiceType varchar(30),
 	ServiceDescription varchar(80)
 );
 
 Create Table ENTRYTYPE
 (
-	EntryTypeID int primary key,
+	EntryTypeID float primary key,
 	EntryType varchar(100),
 	Billable bit
 );
 
 Create Table TIMETYPE
 (
-	TimeTypeID int primary key,
+	TimeTypeID float primary key,
 	TimeType varchar(100),
 	Category varchar(100)
 );
 
 Create Table BILLINGTYPE
 (
-	BillingTypeID int primary key,
+	BillingTypeID float primary key,
 	BillingType varchar(20)
 );
 
-Create Table STATUS
+Create Table WORKORDERSTATUS
 (
-	StatusID Int Primary Key,
+	StatusID float Primary Key,
 	StatusType Varchar(100) 
 );
 
-Create table PRIORITY
+Create table WORKORDERPRIORITY
 (
-	PriorityID int primary key,
+	PriorityID float primary key,
 	PriorityType varchar(100)
 );
 
 CREATE TABLE CONTACT (
-	ContactID int primary key,
+	ContactID float primary key,
 	FirstName varchar(50),
 	LastName varchar(50),
 	EmailAddress nvarchar(70),
@@ -58,32 +60,32 @@ CREATE TABLE CONTACT (
 );
 
 Create Table TBL_GRANT (
-	GrantID int primary key,
+	GrantID float primary key,
 	GrantName Varchar(100),
 	Budget decimal(12,6)
 );
 
 CREATE TABLE SERVICERATE (
-	ServiceRateID int PRIMARY KEY,
+	ServiceRateID float PRIMARY KEY,
 	StartDate date,
 	EndDate date,
-	FiscalYear smallint,
+	FiscalYear varchar(4),
 	Rate decimal(12,6)
 );
 
 CREATE TABLE IPOSTATUS (
-	IPOstatusID int PRIMARY KEY,
+	IPOstatusID float PRIMARY KEY,
 	IPOStatus varchar(30)
 );
 
 CREATE TABLE DEPARTMENT (
-	DepartmentID int primary key,
+	DepartmentID float primary key,
 	DepartmentName varchar(50),
-	CompanyID INT FOREIGN KEY REFERENCES Company(CompanyID)
+	CompanyID float FOREIGN KEY REFERENCES Company(CompanyID)
 );
 
 CREATE TABLE CUSTOMER (
-	CustomerID int primary key,
+	CustomerID float primary key,
 	FirstName varchar(50),
 	LastName varchar(50),
 	EmailAddress nvarchar(70),
@@ -91,12 +93,12 @@ CREATE TABLE CUSTOMER (
 	EmpAddress varchar(100),
 	City varchar(50),
 	EmpState Char(2),
-	ZipCode int,
-	DepartmentID INT REFERENCES DEPARTMENT(DepartmentID)
+	ZipCode varchar(5),
+	DepartmentID float REFERENCES DEPARTMENT(DepartmentID)
 );
 
 CREATE TABLE EMPLOYEE (
-	EmployeeID int primary key,
+	EmployeeID float primary key,
 	FirstName varchar(50),
 	LastName varchar(50),
 	Email nvarchar(70),
@@ -104,62 +106,62 @@ CREATE TABLE EMPLOYEE (
 	EmpAddress varchar(100),
 	City varchar(50),
 	EmpState Char(2),
-	ZipCode int,
-	DepartmentID INT FOREIGN KEY REFERENCES Department(DepartmentID),
-	JobTitleID INT FOREIGN KEY REFERENCES JobTitle(JobTitleID),
-	EmployeeTypeID INT FOREIGN KEY REFERENCES EmployeeType(EmployeeTypeID)
+	ZipCode varchar(5),
+	DepartmentID float FOREIGN KEY REFERENCES Department(DepartmentID),
+	JobTitleID float FOREIGN KEY REFERENCES JobTitle(JobTitleID),
+	EmployeeTypeID float FOREIGN KEY REFERENCES EmployeeType(EmployeeTypeID)
 );
 
 CREATE TABLE IPO (
-	IPOID int PRIMARY KEY,
+	IPOID float PRIMARY KEY,
 	IPODate date,
 	CreationDate date,
 	PaymentDate date,
-	EmployeeID int FOREIGN KEY REFERENCES Employee(EmployeeID),
-	IPOStatusID int FOREIGN KEY REFERENCES IPOSTATUS(IPOStatusID)
+	EmployeeID float FOREIGN KEY REFERENCES Employee(EmployeeID),
+	IPOStatusID float FOREIGN KEY REFERENCES IPOSTATUS(IPOStatusID)
 );
 
 CREATE TABLE IPORATE (
-	IPORateID int PRIMARY KEY,
-	ServiceRateID int FOREIGN KEY REFERENCES IPO(IPOID),
-	IPOID int FOREIGN KEY REFERENCES SERVICERATE(ServiceRateID)
+	IPORateID float PRIMARY KEY,
+	ServiceRateID float FOREIGN KEY REFERENCES IPO(IPOID),
+	IPOID float FOREIGN KEY REFERENCES SERVICERATE(ServiceRateID)
 );
 
 Create table WORKORDER
 (
-	WorkOrderID int primary key,
-	EmployeeID int foreign key references EMPLOYEE(EmployeeID),
-	CustomerID int foreign key references CUSTOMER(CustomerID),
-	PriorityID int foreign key references PRIORITY(PriorityID),
-	StatusID int foreign key references STATUS(StatusID),
+	WorkOrderID float primary key,
+	EmployeeID float foreign key references EMPLOYEE(EmployeeID),
+	CustomerID float foreign key references CUSTOMER(CustomerID),
+	PriorityID float foreign key references WORKORDERPRIORITY(PriorityID),
+	StatusID float foreign key references WORKORDERSTATUS(StatusID),
 	SubmittedDate date,
 	WorkOrderDescription varchar(100),
 	StartDate date,
 	DueDate date,
 	ClosedDate date,
 	Notes varchar(200),
-	GrantID int,
+	GrantID float,
 	ProjectName varchar(100)
 );
 
 Create Table ASSIGNEE
 (
-	AssigneeID int primary key,
-	EmployeeID int foreign key references EMPLOYEE(EmployeeID),
-	WorkOrderID int foreign key references WORKORDER(WorkOrderID)
+	AssigneeID float primary key,
+	EmployeeID float foreign key references EMPLOYEE(EmployeeID),
+	WorkOrderID float foreign key references WORKORDER(WorkOrderID)
 );
 
 
 Create Table TIMESHEET
 (
-	TimeSheetID int primary key,
-	WorkOrderID int foreign key references WORKORDER(WorkOrderID),
-	EmployeeID int foreign key references EMPLOYEE(EmployeeID),
-	EntryTypeID int foreign key references ENTRYTYPE(EntryTypeID),
-	BillingTypeID int foreign key references BILLINGTYPE(BillingTypeID),
-	TimeTypeID int foreign key references TIMETYPE(TimeTypeID),
-	ServiceTypeID int foreign key references SERVICETYPE(ServiceTypeID),
-	IPODID int foreign key references IPO(IPOID),
+	TimeSheetID float primary key,
+	WorkOrderID float foreign key references WORKORDER(WorkOrderID),
+	EmployeeID float foreign key references EMPLOYEE(EmployeeID),
+	EntryTypeID float foreign key references ENTRYTYPE(EntryTypeID),
+	BillingTypeID float foreign key references BILLINGTYPE(BillingTypeID),
+	TimeTypeID float foreign key references TIMETYPE(TimeTypeID),
+	ServiceTypeID float foreign key references SERVICETYPE(ServiceTypeID),
+	IPODID float foreign key references IPO(IPOID),
 	WorkDateTime date,
 	TimeWorkedHours decimal(4,2),
 	WorkPerformed varchar(1000)
@@ -167,9 +169,9 @@ Create Table TIMESHEET
 
 
 CREATE TABLE WORKORDERCONTACT (
-	WorkOrderContactID int primary key,
-	ContactID INT REFERENCES CONTACT(ContactID),
-	WorkOrderID INT REFERENCES WORKORDER(WorkOrderID)
+	WorkOrderContactID float primary key,
+	ContactID float REFERENCES CONTACT(ContactID),
+	WorkOrderID float REFERENCES WORKORDER(WorkOrderID)
 );
 
 
